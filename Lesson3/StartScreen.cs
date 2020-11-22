4a6e44cfb885e975b9bcb92bc9d3b4301e9a8475
heathen29 <diversant329@gmail.com>
2020-11-22 19:31:47 +0200

    Lesson 3. 2 of 3 Tasks.

---
+++ Lesson3/StartScreen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson3
+{
+    class StartScreen
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в сборник задач из Урока №3. Вам на выбор представлены:");
+            Console.WriteLine(
+            "\n" +
+             "1. Cтруктура/класс Complex.\n" +
+             "2. Расчет суммы всех нечетных положительных чисел.\n" +
+             "3. Класс дробей\n" +
+             "\n"
+             );
+            Console.WriteLine("Введите цифру, чтобы продолжить:");
+
+            string choice = Console.ReadLine();
+            Console.WriteLine("\n");
+
+            switch (choice)
+            {
+                case "1":
+                    {
+                        ComplexNumbers.start();
+                        break;
+                    }
+                case "2":
+                    {
+                        OddNumbersSum.start();
+                        break;
+                    }
+                case "3":
+                    {
+                        Fractions.start();
+                        break;
+                    }
+            }
+        }
+    }
+}
