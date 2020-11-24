93213558c400bb7736b717e415c57924b7b90604
heathen29 <diversant329@gmail.com>
2020-11-24 23:14:42 +0200

    Lesson 4. Added templates.

---
+++ Lesson4/Template.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson4
+{
+    /*
+    Выполнил: Юдин Д.
+
+    */
+    class Template
+    {
+        static public void start()
+        {
+            Console.WriteLine("Добро пожаловать в программу ...");
+
+            Console.ReadLine();
+        }
+    }
+}
