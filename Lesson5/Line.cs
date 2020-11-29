4857de29f850b7833e94731cc2e734570e27bf88
heathen29 <diversant329@gmail.com>
2020-11-29 21:21:43 +0200

    Lesson 5. Empty classes for tasks.

---
+++ Lesson5/Line.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson5
+{
+    /*
+    Выполнил: Юдин Д.
+
+    Задание #3
+    */
+
+    /// <summary>
+    /// *Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой.
+    /// Например:badc являются перестановкой abcd.
+    /// </summary>
+    class Line
+    {
+        static public void start()
+        {
+            Console.WriteLine("Добро пожаловать в программу ...");
+
+            Console.ReadLine();
+        }
+    }
+}
